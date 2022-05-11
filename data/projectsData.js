74b2393720c08c4833931acc8c4dad49ff03f18d
Johan Guse <johanguse@gmail.com>
2022-05-10 23:58:08 -0300

    improve projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,96 +4,148 @@ const projectsData = [
     description: `What is you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    imgSrc:
+      'https://res.cloudinary.com/dvcywi3uf/image/upload/v1623864887/jrguse/port/joinrider.jpg',
+    href: 'https://linktr.ee/JoinRiderApp',
+    clientName: 'Joyjet Digital (fr)',
+    clientURL: 'https://www.joyjet.com/',
+    date: 'June 2020 to February 2021',
+    skills: 'React Native, TypeScript, Storybook',
   },
   {
     title: 'cRide',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1623875518/jrguse/port/cride.jpg',
+    href: 'https://www.getcride.com/',
+    clientName: 'Joyjet Digital (fr)',
+    clientURL: 'https://www.joyjet.com/',
+    date: 'February 2021',
+    skills: 'React Native, TypeScript, Storybook',
   },
   {
-    title: 'AmbevTech',
+    title: 'AmbevTech (formerly known as HBSIS)',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc:
+      'https://res.cloudinary.com/dvcywi3uf/image/upload/v1623864887/jrguse/port/ambev-tech.jpg',
+    href: 'https://ambevtech.com.br/',
+    clientName: 'AmbevTech / Hbsis - Sistemas',
+    clientURL: 'https://ambevtech.com.br/',
+    date: 'November 2020',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
-    title: 'HBSIS',
+    title: 'HBSIS (Now AmbevTech)',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1559918631/jrguse/port/hbsis.jpg',
+    href: '',
+    clientName: 'AmbevTech / Hbsis - Sistemas',
+    clientURL: 'https://ambevtech.com.br/',
+    date: 'May 2019',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Protema',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1555250910/jrguse/port/protema.jpg',
+    href: 'https://protema.com.br/',
+    clientName: 'Envolve Labs',
+    clientURL: 'https://envolvelabs.com/',
+    date: 'April 2019',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Maquira',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1534170692/jrguse/port/maquira.jpg',
+    href: 'https://maquira.com.br/',
+    clientName: 'Design Inverso',
+    clientURL: 'https://designinverso.com.br/',
+    date: 'August 2018',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Escola Portal Kids',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1526955681/jrguse/portalkids.jpg',
+    href: '',
+    clientName: 'Portal School',
+    clientURL: 'https://portalschool.com.br/',
+    date: 'Dezember 2017',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Rieper Seguros',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1488550830/jrguse/rieper.jpg',
+    href: 'https://rieper.com.br/',
+    clientName: 'Portal School',
+    clientURL: 'https://portalschool.com.br/',
+    date: 'Dezember 2017',
+    skills: 'PHP, CSS, HTML, JavaScript, WordPress (blog only)',
   },
   {
     title: 'Agência Monograma',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc:
+      'https://res.cloudinary.com/dvcywi3uf/image/upload/v1464783494/jrguse/port/monograma.jpg',
+    href: 'https://www.monogramadesign.com/',
+    clientName: 'Agência Monograma',
+    clientURL: 'https://www.monogramadesign.com/',
+    date: 'January 2016',
+    skills: 'PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Poção Terapêutica',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1464783494/jrguse/port/pocao.jpg',
+    href: 'http://www.pocaoterapeutica.com.br/',
+    clientName: 'E-Sauce',
+    clientURL: 'https://www.esauce.com.br/',
+    date: 'November 2015',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Eiti Gestão de TI',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: 'https://res.cloudinary.com/dvcywi3uf/image/upload/v1464783494/jrguse/port/eiti.jpg',
+    href: 'https://eiti.com.br/',
+    clientName: 'E-Sauce',
+    clientURL: 'https://www.esauce.com.br/',
+    date: 'November 2015',
+    skills: 'WordPress, PHP, CSS, HTML, JavaScript',
   },
   {
     title: 'Cavalo Trucado',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc:
+      'https://res.cloudinary.com/dvcywi3uf/image/upload/v1464783494/jrguse/port/cavalotrucado.jpg',
+    href: 'https://cavalotrucado.com/',
+    clientName: 'Cavalo Trucado',
+    clientURL: 'https://cavalotrucado.com/',
+    date: 'Setember 2015',
+    skills: 'PHP, CSS, HTML, JavaScript',
   },
 ]
 
